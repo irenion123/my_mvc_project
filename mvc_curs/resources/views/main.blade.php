bf97e1f48b7a2b037ca7f0af5a40c4b17c3e3bb6
irenion123 <brotifypacha@gmail.com>
2021-10-24 21:54:08 +0300

    Шаблонизирована домашняя страница

--- mvc_curs/resources/views/main.blade.php
+++ mvc_curs/resources/views/main.blade.php
@@ -3,7 +3,7 @@
     <head>
 
         <meta charset="utf-8">
-        <title>Сайт издательства</title>
+        <title>@yield('title', 'Книги.ру')</title>
 
         <link rel="stylesheet" href="{{ asset('css/main.css') }}"> <!-- Мои стили -->
         <!-- Бибдиотеки BOOTSTRAP и JQuery -->
@@ -16,56 +16,7 @@
     <body>
         @include('header')
         <div class="main">
-            <div class="container mb-4 mt-4">
-                 <h1>Читай. Люби. Развивайся.</h1>
-            <!-- Слайдер -->
-                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                    <div class="carousel-inner">
-
-                        <div class="carousel-item active">
-                            <img src="slider/1.jpg" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="slider/2.jpg" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="slider/3.jpg" class="d-block w-100" alt="...">
-                        </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </div>
-            </div>
-            <!-- Курсы -->
-            <div class="container mb-4 mt-4">
-                <h1>Наши бестселлеры.</h1></br>
-                <div class="row">
-                </div>
-            </div>
-
-            <div class="container mt-4">
-                <div class="row align-items-center">    
-                                            <!-- Контактные данные -->
-                    <div class="col-lg-6 col-12 align-middle">
-                        <div class="contel">
-                            <p>Узнавай первым о новых книгах</p>
-                            <p class="author_text">Все новинки, выпускаемые издательством, Вы сможете увидеть на нашем сайте. Мы будем писать вам раз в месяц о новинках в нашем магазине.</p>
-                        </div>
-                    </div> 
-
-                    <!-- Карта -->
-                    <div class="col-lg-6 col-12 align-middle">
-                        <img src="slider/biblioteka.png" width="550">
-                    </div>
-
-                </div>
-            </div>
+        @yield('main')
         </div>
         @include('footer')
         <script>
