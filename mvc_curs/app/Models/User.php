97de8abcc70c27b67f1ca8a7df5a8b59c352102c
irenion123 <brotifypacha@gmail.com>
2021-11-04 21:55:57 +0300

    Добавляет пользователю атрибует reserved_books

--- mvc_curs/app/Models/User.php
+++ mvc_curs/app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\DB;
 
 class User extends Authenticatable
 {
@@ -34,8 +35,21 @@ class User extends Authenticatable
         'birthday' => 'datetime',
     ];
 
+    protected $appends = [
+        'reserved_books'
+    ];
+
     public function getAuthIdentifierName()
     {
         return 'user_id';
+    }
+
+    public function getReservedBooksAttribute()
+    {
+        $array = DB::select(
+            'select book_id from users_have_books where user_id = :id',
+            ['id' => $this->user_id]
+        );
+        return array_column($array, 'book_id');
     }
 }
