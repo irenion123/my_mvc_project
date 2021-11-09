a42de5cf350d95f88410b314480f0e7f49ac8cc5
irenion123 <brotifypacha@gmail.com>
2021-11-09 20:52:56 +0300

    Добавляет затычку при отсутствии книжек на полке

--- mvc_curs/resources/views/profile_page.blade.php
+++ mvc_curs/resources/views/profile_page.blade.php
@@ -14,12 +14,27 @@
 
     <div class="container mb-4 mt-4">
         <h1><span class="letter">К</span>ниги на полке.</h1></br>
-        <div class="row" style="text-align: left;">
+        <div id="book_shelf" class="row" style="text-align: left;">
         @foreach( $reservedBooks as $book )
             @include('components.book', [ 'book' => $book, 'removable' => true ])
         @endforeach
         </div>
+        <div id="empty_indicator" @class([
+                'text-center',
+                'd-none' => (count($reservedBooks) > 0)
+            ])>
+            <h5>
+                На вашей полке пока пусто, отложите <a href="{{ route('books') }}">книги</a> и они появятся тут
+            </h5>
+        </div>
     </div>
 </div>
+<script>
+$('#book_shelf').bind('DOMSubtreeModified', function(e) {
+    if ($('#book_shelf').children().length < 1) {
+        $('#empty_indicator').removeClass('d-none');
+    }
+});
+</script>
 @endsection
 
