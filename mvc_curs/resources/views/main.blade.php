4ba13a9bda9188dbaa8de856e0ec136d242bb379
irenion123 <brotifypacha@gmail.com>
2021-11-10 20:31:05 +0300

    Повышает приоритет своего css файла

--- mvc_curs/resources/views/main.blade.php
+++ mvc_curs/resources/views/main.blade.php
@@ -5,13 +5,13 @@
         <meta charset="utf-8">
         <title>@yield('title', 'Книги.ру')</title>
 
-        <link rel="stylesheet" href="{{ asset('css/main.css') }}"> <!-- Мои стили -->
         <!-- Бибдиотеки BOOTSTRAP и JQuery -->
         <link rel="stylesheet" href="{{ asset('bootstrap-4.2.1/css/bootstrap.css') }}">
         <script src="{{ asset('js/jquery-3.3.1.js') }}"></script>
         <script src="{{ asset('js/popper.min.js') }} "></script>
         <script src="{{ asset('bootstrap-4.2.1/js/bootstrap.min.js') }}"></script>
         <script src="{{ asset('js/main.js') }}"></script>
+        <link rel="stylesheet" href="{{ asset('css/main.css') }}"> <!-- Мои стили -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
