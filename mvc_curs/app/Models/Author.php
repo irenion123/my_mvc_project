37506adc7f145a74611f34813ebaa486d2a35090
irenion123 <brotifypacha@gmail.com>
2021-11-12 20:48:08 +0300

    Добавляет модель авторов

---
+++ mvc_curs/app/Models/Author.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    protected $table = 'authors';
+    protected $primaryKey = 'author_id';
+}
+
+
+
