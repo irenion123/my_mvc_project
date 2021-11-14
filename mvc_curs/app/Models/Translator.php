0d201278348208db0eef4c60855d7d778bfada1b
irenion123 <brotifypacha@gmail.com>
2021-11-14 21:29:31 +0300

    Добавляет модель переводчика

---
+++ mvc_curs/app/Models/Translator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Translator extends Model
+{
+    protected $table = 'translators';
+    protected $primaryKey = 'translator_id';
+}
+
+
+
