1635f45fc539be12549a4f1767b9add2bd565091
irenion123 <brotifypacha@gmail.com>
2021-12-20 20:14:55 +0300

    Убраны комментарии

--- mvc_curs/app/Http/Controllers/HomePageController.php
+++ mvc_curs/app/Http/Controllers/HomePageController.php
@@ -9,13 +9,8 @@ class HomePageController extends Controller
 
     public function index()
     {
-
         $bestSellers = Book::getBestSellers();
-        // Получаем несколько бестселлеров
-        //
-        // Получаем картинки для карусели
-        //
-        // Рендерим
+
         return view('home_page', [
             'bestSellers' => $bestSellers
         ]);
